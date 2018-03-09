936ff17c4276682d833a8ddf5d967d1d7e40ae5f
John Whiles <johnw@TTL002153.thetrainline.com>
2018-03-09 15:42:38 +0000

    set up basic webserver

--- project.clj
+++ project.clj
@@ -1,6 +1,16 @@
 (defproject clojure-web-server "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :description "A tiny clojure web server"
+  :url ""
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/mit"}
+  :dependencies [
+                 [org.clojure/clojure "1.8.0"]
+                 [ring "1.4.0"]
+                 [compojure "1.3.4"]]
+  :min-lein-version "2.0.0"
+  :uberjar-name "lil.jar"
+  :main clojure-web-server.core
+  :profiles {
+             :dev
+             {:main clojure-web-server.core/-dev-main}}
+  )
--- src/clojure_web_server/core.clj
+++ src/clojure_web_server/core.clj
@@ -1,6 +1,44 @@
-(ns clojure-web-server.core)
+(ns clojure-web-server.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.handler.dump :refer [handle-dump]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]])
+  )
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn welcome
+  "a basic handler function"
+  [request]
+  {:status 200
+   :body "<h1>my first page</h1>"
+   :headers {}})
+
+(defn hello
+  "for saying hello"
+  [request]
+  (let [name (get-in request [:route-params :name])]
+    {:status 200
+     :body (str "<h1> hello " name "</h1>")
+     :headers {}}))
+
+
+(defroutes app
+  (GET "/" [] welcome)
+  (GET "/hello/:name" [] hello)
+  (GET "/request-info" [] handle-dump)
+  (not-found "<h1> nothing here </h1>"))
+
+(defn -main
+  " a little web server"
+  [port-number]
+  (jetty/run-jetty
+    app
+    {:port (Integer. port-number)}))
+
+(defn -dev-main
+  "dev version of the server - with hot reloading"
+  [port-number]
+  (jetty/run-jetty (wrap-reload #'app)
+                   {:port (Integer. port-number)}))
+
